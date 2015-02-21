d2f7dfec9f1c7a0f16cb21edfd061b0909b96664
William Kelly <wikelly@mymail.mines.edu>
2015-02-20 17:41:46 -0700

    filInitTests in progress, added all of Paths to git

---
+++ Paths/src/BoardCell.java
@@ -0,0 +1,21 @@
+
+public class BoardCell {
+
+	private int row, column;
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	
+}
